d2713b63cb70961c58d09edd2471a816ab39ae24
yun <caseyunliu@gmail.com>
2023-08-02 15:15:43 -0400

    setup server

---
+++ index.js
@@ -0,0 +1,31 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { typeDefs } from "./schema.js";
+import db from './_db.js';
+
+// resolvers
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
+
+//server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const {url} = await startStandaloneServer(server, {
+    listen: {port: 4000}
+})
+
+console.log('Server ready at port ', 4000)
